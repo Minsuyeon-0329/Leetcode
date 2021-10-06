cd1ead28568ceddf61c18bce204239a07be05889
Minsuyeon-0329 <63403444+Minsuyeon-0329@users.noreply.github.com>
2021-10-07 00:02:59 +0900

    Time: 152 ms (73.84%), Space: 16.5 MB (75.20%) - LeetHub

---
+++ max-stack/max-stack.py
@@ -0,0 +1,36 @@
+class MaxStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+
+    def popMax(self) -> int:
+        m = max(self.stack)
+        s = self.stack[::-1]
+        s.remove(m)
+        self.stack = s[::-1]
+        return m
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
